a9fb4ef48938067369512dbeea6385bfa06b8cd6
Patel <a_patel73563@tss-hr420-dm27.fc.ca>
2019-03-14 17:40:02 -0400

    inclass build done

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = {
+    name: "Abhi",
+    role: "student",
+
+    user1: {
+        name: "Abhi",
+        role: "Designer",
+        bio: "Designer at Fanshawe",
+        bioPic: "user1.jpg"
+    },
+
+    user2: {
+        name: "Abhi",
+        role: "Developer",
+        bio: "Developer at Fanshawe",
+        bioPic: "user2.jpg"
+    }
+}
+
+module.exports = data;
